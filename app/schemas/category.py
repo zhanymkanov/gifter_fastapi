fc6964bcecfa3f5eec01acf60019dfc650c8c7f5
n1EzeR <erasylz@gmail.com>
2020-06-16 22:38:02 +0600

    Make Category Schema Base fields optional by default
    DRY

--- app/schemas/category.py
+++ app/schemas/category.py
@@ -5,24 +5,25 @@ from pydantic import BaseModel
 
 # Shared properties
 class CategoryBase(BaseModel):
-    title: str
-    slug: str
-    parent_id: Optional[str] = None
-
-
-class CategoryCreate(CategoryBase):
-    pass
-
-
-class CategoryUpdate(CategoryBase):
     title: Optional[str] = None
     slug: Optional[str] = None
     parent_id: Optional[str] = None
 
 
+class CategoryCreate(CategoryBase):
+    title: str
+    slug: str
+
+
+class CategoryUpdate(CategoryBase):
+    pass
+
+
 # Base Properties for models stored in DB
 class CategoryInDBBase(CategoryBase):
     id: Any
+    title: str
+    slug: str
 
     class Config:
         orm_mode = True

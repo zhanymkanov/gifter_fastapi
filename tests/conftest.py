2631fd89ec9657ad4e9f70f95e8afb4385bfa390
n1EzeR <erasylz@gmail.com>
2020-06-16 22:44:25 +0600

    Init conftests
    - Testing DB
    - Fixtures for client and db session

---
+++ tests/conftest.py
@@ -0,0 +1,28 @@
+from typing import Generator
+
+import pytest
+from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from app.core.config import settings
+from app.db.base import Base
+from app.main import app
+
+TEST_DATABASE_URL = settings.TEST_DATABASE_URI
+
+engine = create_engine(TEST_DATABASE_URL)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
+
+
+@pytest.fixture(scope="session")
+def db() -> Generator:
+    yield TestingSessionLocal()
+
+
+@pytest.fixture(scope="module")
+def client() -> Generator:
+    with TestClient(app) as c:
+        yield c

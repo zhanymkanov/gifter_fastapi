c95917046adb3749d04c9109e0e4e643a77dfc3a
n1EzeR <erasylz@gmail.com>
2020-06-19 20:35:44 +0600

    Use predefined JWT_SECRET

--- app/config.py
+++ app/config.py
@@ -1,12 +1,10 @@
-import secrets
-
 from starlette.config import Config
 
 config = Config(".env")
 
 JWT_EXP: int = 60 * 24 * 7  # 7 days
 JWT_ALG: str = config("ALGORITHM")
-JWT_SECRET: str = secrets.token_urlsafe(32)
+JWT_SECRET: str = config("SECRET_KEY")
 
 SQLALCHEMY_DATABASE_URI = config("DATABASE_URL")
 TEST_DATABASE_URI = config("TEST_DATABASE_URL")

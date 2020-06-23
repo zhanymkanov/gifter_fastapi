29881ccc219e8ee2c0ba223b1d607341daa17e8d
n1EzeR <erasylz@gmail.com>
2020-06-23 13:40:44 +0600

    Add Auth login/register/read_me endpoints

--- app/main.py
+++ app/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
 
 from app.database import Base, engine
+from app.auth.router import router as auth_router
 
 Base.metadata.create_all(engine)
 
 app = FastAPI()
+
+app.include_router(auth_router, prefix="/auth")

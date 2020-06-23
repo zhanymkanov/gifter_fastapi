9b02a2f7b996c4f301d4ddab2ba65425fd62dadf
n1EzeR <erasylz@gmail.com>
2020-06-23 14:04:13 +0600

    Fix Users tests
    Users CRUD was moved to the separate module form services

--- tests/auth/test_auth_service.py
+++ tests/auth/test_auth_service.py
@@ -1,50 +1,15 @@
 from sqlalchemy.orm import Session
 
-from app.auth import security, service
-from app.auth.models import UserRegister, UserLogin
+from app.auth import service, users
+from app.auth.models import UserLogin, UserRegister
 from tests.utils import random_lower_string
-
-
-def test_user_create(db: Session):
-    user_email = f"{random_lower_string()}@mail.net"
-    user_password = random_lower_string()
-    user_in = UserRegister(email=user_email, password=user_password)
-    user = service.create(db=db, user_in=user_in)
-
-    assert user.id
-    assert user.email == user_email
-    assert security.verify_password(user_password, user.password)
-
-
-def test_user_get(db: Session):
-    user_email = f"{random_lower_string()}@mail.net"
-    user_password = random_lower_string()
-    user_in = UserRegister(email=user_email, password=user_password)
-    user = service.create(db=db, user_in=user_in)
-
-    user_stored = service.get(db=db, user_id=user.id)
-    assert user_stored.id
-    assert user_stored.email == user_email
-    assert security.verify_password(user_password, user_stored.password)
-
-
-def test_user_get_by_email(db: Session):
-    user_email = f"{random_lower_string()}@mail.net"
-    user_password = random_lower_string()
-    user_in = UserRegister(email=user_email, password=user_password)
-
-    service.create(db=db, user_in=user_in)
-    user_stored = service.get_by_email(db=db, email=user_email)
-    assert user_stored.id
-    assert user_stored.email == user_email
-    assert security.verify_password(user_password, user_stored.password)
 
 
 def test_authenticate(db: Session):
     user_email = f"{random_lower_string()}@mail.net"
     user_password = random_lower_string()
     user_in = UserRegister(email=user_email, password=user_password)
-    service.create(db=db, user_in=user_in)
+    users.create(db=db, user_in=user_in)
 
     user_in = UserLogin(email=user_email, password=user_password)
     logged_in = service.authenticate(db=db, user_in=user_in)
@@ -55,7 +20,7 @@ def test_authenticate_not_exists(db: Session):
     user_email = f"{random_lower_string()}@mail.net"
     user_password = random_lower_string()
     user_in = UserRegister(email=user_email, password=user_password)
-    service.create(db=db, user_in=user_in)
+    users.create(db=db, user_in=user_in)
 
     user_in = UserLogin(email="RANDOM_USER@404.me", password=user_password)
     logged_in = service.authenticate(db=db, user_in=user_in)
@@ -66,7 +31,7 @@ def test_authenticate_bad_credentials(db: Session):
     user_email = f"{random_lower_string()}@mail.net"
     user_password = random_lower_string()
     user_in = UserRegister(email=user_email, password=user_password)
-    service.create(db=db, user_in=user_in)
+    users.create(db=db, user_in=user_in)
 
     user_in = UserLogin(email=user_email, password="HACK_YOU")
     logged_in = service.authenticate(db=db, user_in=user_in)

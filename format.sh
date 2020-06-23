0cff8d2044ab07736d7552db7c4797a69a291917
n1EzeR <erasylz@gmail.com>
2020-06-23 14:05:10 +0600

    Add tests to formatting scripts

--- format.sh
+++ format.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 set -x
 
-autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place app --exclude=__init__.py
-isort --recursive --apply app
-black app
+autoflake --remove-all-unused-imports --recursive --remove-unused-variables --in-place app tests --exclude=__init__.py
+isort --recursive --apply app tests
+black app tests

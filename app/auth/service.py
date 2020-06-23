229bdef3597a4d497bef299c151ae9df58806f90
n1EzeR <erasylz@gmail.com>
2020-06-23 13:40:09 +0600

    Add JWT token parser for usernames

--- app/auth/service.py
+++ app/auth/service.py
@@ -1,36 +1,51 @@
-from typing import Any
-
-from fastapi import HTTPException, status
+import jwt
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 
+from app.config import JWT_ALG, JWT_SECRET
+from app.database import get_db
+
+from . import users
 from .constants import ErrorCode
-from .models import User, UserLogin, UserRegister
-from .security import hash_password, verify_password
+from .models import TokenData, User, UserLogin
+from .security import verify_password
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/token")
 
 
-def get(db: Session, user_id: Any):
-    return db.query(User).filter(User.id == user_id).first()
+async def get_jwt_user(
+        db: Session = Depends(get_db),
+        token: str = Depends(oauth2_scheme)
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail=ErrorCode.COULD_NOT_VALIDATE_CREDENTIALS,
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = jwt.decode(token, JWT_SECRET, algorithms=[JWT_ALG])
+    except jwt.PyJWTError:
+        raise credentials_exception
 
+    email: str = payload.get("sub")
+    if not email:
+        raise credentials_exception
 
-def get_by_email(db: Session, email: str):
-    return db.query(User).filter(User.email == email).first()
+    token_data = TokenData(email=email)
+    user = users.get_by_email(db, email=token_data.email)
+    if not user:
+        raise credentials_exception
 
-
-def create(*, db: Session, user_in: UserRegister) -> User:
-    user = get_by_email(db, user_in.email)
-    if user:
-        raise HTTPException(
-            status_code=status.HTTP_400_BAD_REQUEST,
-            detail=ErrorCode.REGISTER_USER_ALREADY_EXISTS,
-        )
-    plain_password = user_in.password.get_secret_value()
-    hashed_password = hash_password(plain_password)
-    user = User(**user_in.dict(exclude={"password"}), password=hashed_password)
-
-    db.add(user)
-    db.commit()
-    db.refresh(user)
     return user
+
+
+async def get_jwt_user_active(current_user: User = Depends(get_jwt_user)):
+    if not current_user.is_active:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST, detail=ErrorCode.USER_IS_NOT_ACTIVE
+        )
+    return current_user
 
 
 def authenticate(*, db: Session, user_in: UserLogin):

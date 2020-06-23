05afa2861775be28a90d44cb54872b51dbc22ed0
n1EzeR <erasylz@gmail.com>
2020-06-23 14:34:39 +0600

    Add new user fixture

--- tests/auth/test_auth_service.py
+++ tests/auth/test_auth_service.py
@@ -1,7 +1,7 @@
 from sqlalchemy.orm import Session
 
 from app.auth import service, users
-from app.auth.models import UserLogin, UserRegister
+from app.auth.models import User, UserLogin, UserRegister
 from tests.utils import random_lower_string
 
 
@@ -17,22 +17,12 @@ def test_authenticate(db: Session):
 
 
 def test_authenticate_not_exists(db: Session):
-    user_email = f"{random_lower_string()}@mail.net"
-    user_password = random_lower_string()
-    user_in = UserRegister(email=user_email, password=user_password)
-    users.create(db=db, user_in=user_in)
-
-    user_in = UserLogin(email="RANDOM_USER@404.me", password=user_password)
+    user_in = UserLogin(email="not_exists@404.me", password="invalid_password")
     logged_in = service.authenticate(db=db, user_in=user_in)
     assert not logged_in
 
 
-def test_authenticate_bad_credentials(db: Session):
-    user_email = f"{random_lower_string()}@mail.net"
-    user_password = random_lower_string()
-    user_in = UserRegister(email=user_email, password=user_password)
-    users.create(db=db, user_in=user_in)
-
-    user_in = UserLogin(email=user_email, password="HACK_YOU")
+def test_authenticate_bad_credentials(db: Session, user: User):
+    user_in = UserLogin(email=user.email, password="invalid_password")
     logged_in = service.authenticate(db=db, user_in=user_in)
     assert not logged_in
--- tests/conftest.py
+++ tests/conftest.py
@@ -3,11 +3,14 @@ from typing import Generator
 import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session, sessionmaker
 
+from app.auth import users
+from app.auth.models import User, UserRegister
 from app.config import TEST_DATABASE_URI
 from app.database import Base
 from app.main import app
+from tests.utils import random_lower_string
 
 engine = create_engine(TEST_DATABASE_URI)
 TestingSessionLocal = sessionmaker(bind=engine)
@@ -24,3 +27,13 @@ def db() -> Generator:
 def client() -> Generator:
     with TestClient(app) as c:
         yield c
+
+
+@pytest.fixture(scope="module")
+def user(db: Session) -> User:
+    user_email = f"{random_lower_string()}@mail.net"
+    user_password = random_lower_string()
+    user_in = UserRegister(email=user_email, password=user_password)
+
+    user = users.create(db=db, user_in=user_in)
+    return user

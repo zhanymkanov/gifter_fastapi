fd571a8bd2e4532644718c89316926c59b7f521c
n1EzeR <erasylz@gmail.com>
2020-06-24 12:46:26 +0600

    Move review to the product package

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from app.auth.models import User  # noqa
-from app.product.category.models import Category  # noqa
 from app.order.models import Order, OrderProducts  # noqa
-from app.product.models import Image, Product, Review  # noqa
+from app.product.category.models import Category  # noqa
+from app.product.models import Image, Product  # noqa
+from app.product.review.models import Review  # noqa
 from app.profile.models import Profile  # noqa
 from app.shop.models import Employee, Shop  # noqa
--- app/product/models.py
+++ app/product/models.py
@@ -22,6 +22,7 @@ if TYPE_CHECKING:
     from app.auth.models import User  # noqa
     from app.order.models import Order  # noqa
     from app.product.category.models import Category  # noqa
+    from app.product.review.models import Review  # noqa
     from app.shop.models import Shop  # noqa
 
 
@@ -42,19 +43,6 @@ class Product(Base, TimeStampMixin):
     images = relationship("Image", back_populates="product")
     reviews = relationship("Review", back_populates="product")
     orders = relationship("Order", back_populates="products", secondary=OrderProducts)
-
-
-class Review(Base, TimeStampMixin):
-    id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    rating = Column(SmallInteger, index=True)
-    plus = Column(String)
-    minus = Column(String)
-    comment = Column(String)
-    product_id = Column(UUID, ForeignKey("product.id"))
-    user_id = Column(UUID, ForeignKey("users.id"))
-
-    product = relationship("Product", back_populates="reviews", uselist=False)
-    user = relationship("User", back_populates="reviews", uselist=False)
 
 
 class Image(Base, TimeStampMixin):
